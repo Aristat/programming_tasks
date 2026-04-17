7a18e9e3f748449ddb04c1d13315310b98fd2405
Alexander Strelnikov <aristat37@gmail.com>
2026-04-17 22:15:52 +0300

    palindrome-number

---
+++ typescript/palindrome-number.ts
@@ -0,0 +1,7 @@
+function isPalindrome(x: number): boolean {
+    return x.toString().split('').reverse().join('') == x.toString()
+}
+
+console.log(isPalindrome(121))
+console.log(isPalindrome(-121))
+console.log(isPalindrome(10))
