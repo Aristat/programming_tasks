751f8f7476913dc0f29ee7afc3ee8b2f11f94349
Alexander Strelnikov <aristat37@gmail.com>
2026-08-04 12:45:55 +0300

    group anagrams

---
+++ typescript/group-anagrams.ts
@@ -0,0 +1,16 @@
+function groupAnagrams(strs: string[]): string[][] {
+    const map = new Map()
+    for (let str of strs) {
+        const arr: number[] = new Array(26)
+        for (let char of str) {
+            arr[char.charCodeAt(0) - 97] ||= 0
+            arr[char.charCodeAt(0) - 97]++
+        }
+
+        const uniqStr: string = arr.join(',')
+        map[uniqStr] ||= []
+        map[uniqStr]!.push(str)
+    }
+
+    return Object.values(map)
+};
