9674c64ea575797be0f5e08d2d3ce058a0dd0d9c
Alexander Strelnikov <aristat37@gmail.com>
2026-08-04 11:13:15 +0300

    valid anagram

---
+++ typescript/valid-anagram.ts
@@ -0,0 +1,16 @@
+function isAnagram(s: string, t: string): boolean {
+    const sMap = new Map()
+    const tMap = new Map()
+    if (s.length != t.length) return false
+
+    for (let i = 0; i < s.length; i++) {
+        sMap.set(s[i], (sMap.get(s[i]) || 0) + 1)
+        tMap.set(t[i], (tMap.get(t[i]) || 0) + 1)
+    }
+
+    for (const [key, value] of sMap) {
+        if (tMap.get(key) != value) return false
+    }
+
+    return true
+};
