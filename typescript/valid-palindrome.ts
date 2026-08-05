45106689cca3c32fb4dfed2e1cc46b02009575c3
Alexander Strelnikov <aristat37@gmail.com>
2026-08-05 12:26:17 +0300

    valid palindrome

---
+++ typescript/valid-palindrome.ts
@@ -0,0 +1,27 @@
+function isPalindrome(s: string): boolean {
+    if (s.length === 0) return true;
+
+    let cleanS: string = '';
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i].toLowerCase()
+        const charCode: number = char.charCodeAt(0)
+        if ((charCode >= 48 && charCode <= 57) || (charCode >= 97 && charCode <= 122) ) {
+            cleanS += char
+        }
+    }
+
+    let left: number = 0;
+    let right: number = cleanS.length - 1;
+    let valid: boolean = true;
+    for (let i = 0; i < Math.floor(cleanS.length / 2); i++) {
+        if (cleanS[left] != cleanS[right]) {
+            valid = false
+            break
+        }
+
+        left++
+        right--
+    }
+
+    return valid
+};
