6bf5235da249ca8602eda2112b1e54eccfed0128
Alexander Strelnikov <aristat37@gmail.com>
2026-08-05 09:50:21 +0300

    top-k frequent elements

---
+++ typescript/top-k-frequent-elements.ts
@@ -0,0 +1,9 @@
+function topKFrequent(nums: number[], k: number): number[] {
+    const map = new Map()
+    for (let i = 0; i < nums.length; i++) {
+        map.set(nums[i], (map.get(nums[i]) || 0) + 1)
+    }
+
+    const sorted = [...map.entries()].sort((a, b) => b[1] - a[1])
+    return sorted.slice(0, k).map(([key]) => key)
+};
